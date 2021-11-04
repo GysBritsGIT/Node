67728d59738a23fb6e294ba8df6d1ea1f14c03ca
GysBritsGIT <91754356+GysBritsGIT@users.noreply.github.com>
2021-11-04 10:26:05 +0200

    commit

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
 
+app.set('views','./src/views');
+app.set('view engine', 'ejs');
+
 app.get('/',(req, res)=>{
-res.send('Hello from my app');
+res.render('index', {title: 'Gallery App', data:['a', 'b', 'c'] });
 
 });
 
 app.listen(3000,()=>{
-debug(`Listen on port ${chalk.green('3000')}`);
+debug(`Listen on port ${chalk.green(PORT)}`);
 });
